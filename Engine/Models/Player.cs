b2581c290c9b70a60aa49741b816053691fb7171
Paul Thurmond <paulthurmond@gmail.com>
2020-06-05 15:41:26 -0500

    Lesson 03.3

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    class Player
+    {
+        string Name { get; set; }
+        string CharacterClass { get; set; }
+        int HitPoints { get; set; }
+        int ExperiencePoints { get; set; }
+        int Level { get; set; }
+        int Gold { get; set; }
+
+    }
+}
