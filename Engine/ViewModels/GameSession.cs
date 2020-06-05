8ec995d9b810b993853ad77be2ce38c8f7cd0697
Paul Thurmond <paulthurmond@gmail.com>
2020-06-05 16:50:25 -0500

    Lesson 03.4

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,20 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    class GameSession
+    {
+        Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Rotwang";
+            CurrentPlayer.Gold = 1_000_000;
+
+        }
+    }
+}
